236bf5f9688307f231480a6076360a9b4937c89a
agentheath <heath.reinhard@gmail.com>
2021-09-12 12:54:44 -0700

    Initial commit

---
+++ Infinite Loops1.py
@@ -0,0 +1,14 @@
+
+while True:
+    input_a = input("Do you like infinite loops?\nYes or no?\n")
+    
+    while True:
+        if input_a.lower().startswith("y"):    
+            print("Me too!!!")
+        
+        elif input_a.lower().startswith("n"):
+            print("Too bad!!!")
+        
+        else:
+            print("\nwut\n")
+            break
